bbdf64fd2681b4c00d1abe796ff8021498e0b93b
MFRamon <ramon.manfig@gmail.com>
2024-01-18 09:53:16 -0600

    Adding configuration for dark theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,21 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import type { AppProps } from 'next/app'
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline/>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
