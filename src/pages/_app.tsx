a7eca583b03f0bc1013a1a7e074947838f9736f1
MFRamon <ramon.manfig@gmail.com>
2024-01-23 23:24:32 -0600

    Dark mode based on system or OS settings

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,18 +4,26 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import useMediaQuery from '@mui/material/useMediaQuery';
 import CssBaseline from "@mui/material/CssBaseline";
 import type { AppProps } from "next/app";
-
-const darkTheme = createTheme({
-  palette: {
-    mode: "light",
-  },
-});
+import { useMemo } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
+
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Component {...pageProps} />
     </ThemeProvider>
