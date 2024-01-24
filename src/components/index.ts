36525138d4b8c54ad40b9bb68c85f30168e68cdb
MFRamon <ramon.manfig@gmail.com>
2024-01-23 19:51:35 -0600

    Three Shaking for component imports

---
+++ src/components/index.ts
@@ -0,0 +1,17 @@
+import CompletedTasksList from "./CompletedTasksList/CompletedTasksList";
+import LogoHeader from "./LogoHeader/LogoHeader";
+import MetaHead from "./MetaHead/MetaHead";
+import SelectedTaskDetail from "./SelectedTaskDetail/SelectedTaskDetail";
+import TasksFilters from "./TasksFilters/TasksFilters";
+import TasksTableToolbar from "./TasksTableToolbar/TasksTableToolbar";
+import Timer from "./Timer/Timer";
+
+export {
+    CompletedTasksList,
+    LogoHeader,
+    MetaHead,
+    SelectedTaskDetail,
+    TasksFilters,
+    TasksTableToolbar,
+    Timer
+}
